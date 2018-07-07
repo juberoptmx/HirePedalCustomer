f5cd4ec1ea65197355a4851b6d7d630a57e15324
Juber Moulvi Abdul <ma.juber@gmail..com>
2018-07-07 22:09:11 +0530

    Code cleanup

--- app/src/main/java/com/hirepedal/customer/cart/CartFragment.kt
+++ app/src/main/java/com/hirepedal/customer/cart/CartFragment.kt
@@ -29,74 +29,74 @@ class CartFragment: BaseFragment(), CartListener {
     private fun getCartItems() {
 
         // Hercules
-        cartItemList.add(CartItem("Hercules", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hercules),
+        cartItemList.add(CartItem("Sangam Cycle Shop", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Roadsters", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hercules_c1),
                 26.9510036, 75.7577134))
-        cartItemList.add(CartItem("Hercules", 2, "Jayanagar", resources.getDrawable(R.drawable.ic_hercules),
+        cartItemList.add(CartItem("Bumson Saddle Shop", 2, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Roadeo", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hercules_c1),
                 26.903857, 75.821208))
-        cartItemList.add(CartItem("Hercules", 3, "Jayanagar", resources.getDrawable(R.drawable.ic_hercules),
+        cartItemList.add(CartItem("Jayant Cycles", 3, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Ryders", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hercules_c1),
                 26.9359042, 75.7114598))
-        cartItemList.add(CartItem("Hercules", 4, "Jayanagar", resources.getDrawable(R.drawable.ic_hercules),
+        cartItemList.add(CartItem("Altaf Cycles", 4, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Turbodrive MTB", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hercules_c1),
                 26.996827, 75.902208))
-        cartItemList.add(CartItem("Hercules", 5, "Jayanagar", resources.getDrawable(R.drawable.ic_hercules),
+        cartItemList.add(CartItem("Five Start Cycles", 5, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "CMX", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hercules_c1),
                 26.829615, 75.644113))
 
         // BSA
-        cartItemList.add(CartItem("BSA", 6, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa),
+        cartItemList.add(CartItem("Sai Cycles", 6, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Champ", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa_c1),
                 26.943993, 75.809052))
-        cartItemList.add(CartItem("BSA", 7, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa),
+        cartItemList.add(CartItem("VST Cycles", 7, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Toddlers", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa_c1),
                 26.996200, 75.828222))
-        cartItemList.add(CartItem("BSA", 7, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa),
+        cartItemList.add(CartItem("Aabheer Cycles", 7, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Ladybird", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa_c1),
                 26.938494, 75.632905))
-        cartItemList.add(CartItem("BSA", 8, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa),
+        cartItemList.add(CartItem("Chitranjan Cycles", 8, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Workouts", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa_c1),
                 26.929836, 75.840632))
-        cartItemList.add(CartItem("BSA", 9, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa),
+        cartItemList.add(CartItem("Gajendra Cycles", 9, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Jr. Roadsters", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa_c1),
                 27.030329, 75.623733))
-        cartItemList.add(CartItem("BSA", 10, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa),
+        cartItemList.add(CartItem("Kanhaiya Lal Cycles", 10, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Roadsters", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_bsa_c1),
                 26.898083, 75.895666))
 
 
 
         // Hero Cycles
-        cartItemList.add(CartItem("Hero Cycles", 11, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_cycles),
+        cartItemList.add(CartItem("Madhukar Cycles", 11, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Maxim Fun Series", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_c1),
                 26.898083, 75.752146))
 
-        cartItemList.add(CartItem("Hero Cycles", 12, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_cycles),
+        cartItemList.add(CartItem("Radheyshyam Cycles", 12, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Special Kidz Bikes", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_c1),
                 26.913960, 75.839553))
-        cartItemList.add(CartItem("Hero Cycles", 13, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_cycles),
+        cartItemList.add(CartItem("Subhash Cycles", 13, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Super Start Series", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_c1),
                 27.066849, 75.602690))
-        cartItemList.add(CartItem("Hero Cycles", 14, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_cycles),
+        cartItemList.add(CartItem("Vikrant Cycles", 14, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "SLR", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_c1),
                 26.903291, 75.812023))
-        cartItemList.add(CartItem("Hero Cycles", 15, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_cycles),
+        cartItemList.add(CartItem("Vivek Cycles", 15, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Ranger MTB/ATB", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_c1),
                 26.9068372, 75.7992979))
-        cartItemList.add(CartItem("Hero Cycles", 16, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_cycles),
+        cartItemList.add(CartItem("Purshottam Cycles", 16, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "City Bikes", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_c1),
                 26.872220, 75.812781))
-        cartItemList.add(CartItem("Hero Cycles", 17, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_cycles),
+        cartItemList.add(CartItem("Jawahar Cycles", 17, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Roadsters", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_c1),
                 26.929565, 75.757142))
-        cartItemList.add(CartItem("Hero Cycles", 18, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_cycles),
+        cartItemList.add(CartItem("Hanumant Cycles", 18, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Hero Sprint", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_c1),
                 26.961072, 75.846072))
-        cartItemList.add(CartItem("Hero Cycles", 19, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_cycles),
+        cartItemList.add(CartItem("Hariram Cycles", 19, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "ATB", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_c1),
                 26.928752, 26.928752))
-        cartItemList.add(CartItem("Hero Cycles", 19, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_cycles),
+        cartItemList.add(CartItem("Amanpreet Cycles", 19, "Jayanagar", resources.getDrawable(R.drawable.ic_cycle),
                 "Master Blasters", 1, "Jayanagar", resources.getDrawable(R.drawable.ic_hero_c1),
                 26.880966, 75.752126))
 
